2dacafdbf94b77bfd6d71ffcaf261147686bcc16
helenahagman <>
2023-09-27 10:10:49 +0000

    added home page content

--- bay_lake_winery/settings.py
+++ bay_lake_winery/settings.py
@@ -60,7 +60,10 @@ ROOT_URLCONF = 'bay_lake_winery.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
