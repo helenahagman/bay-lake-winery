0f8495a3cd5eab82a10109cba1f7ef319f6a7199
helenahagman <>
2023-10-03 09:42:35 +0000

    added product_detail.html and product.html

--- products/urls.py
+++ products/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.all_products, name='products'),
+    path('product/<int:product_id>/', views.product_detail, name='product_detail'),
 ]
