40c8291e9222066d8e1d3c0589a41cf7acec32bc
helenahagman <>
2023-10-09 12:07:54 +0000

    added quantity input

--- shopbag/contexts.py
+++ shopbag/contexts.py
@@ -11,27 +11,16 @@ def shopbag_contents(request):
     product_count = 0
     shopbag = request.session.get('shopbag', {})
 
-    for item_id, item_data in shopbag.items():
-        if isinstance(item_data, int):
-            product = get_object_or_404(Product, pk=item_id)
-            total += item_data * product.price
-            product_count += item_data
-            shopbag_items.append({
-                'item_id': item_id,
-                'quantity': item_data,
-                'product': product,
-            })
-        else:
-            product = get_object_or_404(Product, pk=item_id)
-            for size, quantity in item_data['items_by_size'].items():
-                total += quantity * product.price
-                product_count += quantity
-                shopbag_items.append({
-                    'item_id': item_id,
-                    'quantity': quantity,
-                    'product': product,
-                    })
-
+    for item_id, quantity in shopbag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += item_data * product.price
+        product_count += quantity
+        shopbag_items.append({
+            'item_id': item_id,
+            'quantity': item_data,
+            'product': product,
+        })
+        
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
