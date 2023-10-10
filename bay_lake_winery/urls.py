e53511b42bc1c48e6a7f91569eae2e54f3cab78a
helenahagman <>
2023-10-10 11:19:35 +0000

    moved media folder to static folder

--- bay_lake_winery/urls.py
+++ bay_lake_winery/urls.py
@@ -25,9 +25,9 @@ urlpatterns = [
     path('', include('home.urls')),
     path('products/', include('products.urls')),
     path('shopbag/', include('shopbag.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
 
 # Serve media files during development
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
