1536aecbdc23bd13526a371deb940d245deeceb5
helenahagman <>
2023-10-16 13:51:49 +0000

    added style to sign-up and verification html pages.

--- shopbag/urls.py
+++ shopbag/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.view_shopbag, name='view_shopbag'),
+    path('shopbag/', views.view_shopbag, name='view_shopbag'),
     path('add/<item_id>/', views.add_to_shopbag, name='add_to_shopbag'),
     path('adjust/<item_id>/', views.adjust_shopbag, name='adjust_shopbag'),
     path('remove/<item_id>/', views.remove_from_shopbag, name='remove_from_shopbag'),
