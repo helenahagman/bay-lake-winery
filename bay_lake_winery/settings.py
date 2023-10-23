26d54f51f2e4f5b8d73b811bac1730afe7f6eee2
helenahagman <>
2023-10-23 15:14:56 +0000

    trying to correct error in images not showing in shopping bag

--- bay_lake_winery/settings.py
+++ bay_lake_winery/settings.py
@@ -187,9 +187,6 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'media')
 
-# MEDIA_URL = '/products/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'products')
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
