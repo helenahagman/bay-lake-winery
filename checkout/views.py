de9dce9e95b0804c1d5d4c25a0b8f1940db160ca
helenahagman <>
2023-10-24 09:17:35 +0000

    installed stripe

--- checkout/views.py
+++ checkout/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 
 from .forms import OrderForm
+from shopbag.contexts import shopbag_contents 
 
 
 def checkout_view(request):
@@ -10,10 +11,14 @@ def checkout_view(request):
         messages.error(request, 'Your bag is empty')
         return redirect(reverse('products'))
 
+    current_shopbag = shopbag_contents(request)
+    total = current_shopbag['total']
+    stripe_total = round(total * 100)
+    
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
-        'order_form': order_form
+        'order_form': order_form,
         'stripe_public_key': 'pk_test_51O4fn8ISFE7U70bBbKXW3bjI23jy4FQOAAgEUKF5BdQ9bEoBu6LfEIuA0SduI4vvPvMuZlmmcDCJhkPRjZuvMGaf003jmx6ZpY',
         'client_secret': 'test client secret',
     }
