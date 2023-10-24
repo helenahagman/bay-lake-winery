faafd0a90917cbe881bdbc11c946513fe1e8ec8a
helenahagman <>
2023-10-24 11:42:32 +0000

    updated stripe_elements.js to get the cc input field to display on the checkout page

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -5,11 +5,10 @@
     CSS from here: 
     https://stripe.com/docs/stripe-js
 */
-
-var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
-var clientSecret = $('#id_client_secret').text().slice(1, -1);
-var stripe = stripe(stripePublicKey);
+var stripe = Stripe('{{ id_stripe_public_key|safe }}');
 var elements = stripe.elements();
+var cardElement = elements.create('card');
+cardElement.mount('#card-element');
 var style = {
     base: {
         color: '#000',
