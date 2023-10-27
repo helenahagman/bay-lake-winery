3f62b922f6103e6a0e08ce92871f7c5f84a2c48b
helenahagman <>
2023-10-27 18:51:17 +0000

    product admin - added security to view

--- products/views.py
+++ products/views.py
@@ -79,12 +79,16 @@ def product_detail(request, product_id):
 @login_required
 def add_product(request):
     """ Add a new product """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
     if request.method == 'POST':
         form = ProductForm(request.POST)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Product added')
-            return redirect(reverse('products'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'No product added')
     else:
@@ -99,6 +103,10 @@ def add_product(request):
 @login_required
 def edit_product(request, product_id):
     """ Edit an existing product """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
     product = get_object_or_404(Product, pk=product_id)
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES, instance=product)
@@ -128,6 +136,10 @@ def edit_product(request, product_id):
 @login_required
 def delete_product(request, product_id):
     """ Delete a product """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+        
     product = get_object_or_404(Product, pk=product_id)
     product.delete()
     messages.success(request, 'Product removed')
