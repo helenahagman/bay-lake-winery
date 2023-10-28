a328d4de1a70ac6ccf0f0fc0b2baf535ee1cb45e
helenahagman <>
2023-10-28 18:22:11 +0000

    updated settings.py

--- bay_lake_winery/settings.py
+++ bay_lake_winery/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+if os.path.isfile("env.py"):
+    import env
 import dj_database_url
 
 from decouple import config
