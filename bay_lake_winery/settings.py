dc406e522645ffd38adb9a2df8a86d40c10343a2
helenahagman <>
2023-10-28 11:50:49 +0000

    updated settings.py

--- bay_lake_winery/settings.py
+++ bay_lake_winery/settings.py
@@ -27,12 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = [
-    'django-insecure-8*=n245yw&%ae1cdfi-r$3_xl)vzga^eg4)%!m$9c*fp%g_tln'
-]
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     '8000-helenahagman-bay-lake-wi-tcijqtegyu.us2.codeanyapp.com',
