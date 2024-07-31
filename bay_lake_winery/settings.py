6bf2b44fb73386e5ad40d81fee7c63ff528a23f4
helenahagman <helena@rapibo.se>
2024-07-31 08:36:51 +0000

    updated settings file

--- bay_lake_winery/settings.py
+++ bay_lake_winery/settings.py
@@ -121,18 +121,6 @@ AUTHENTICATION_BACKENDS = (
 
 SITE_ID = 1
 
-if 'DEVELOPMENT' in os.environ:
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'baylakewinery@mail.com'
-else:
-    EMAIL_BACKEND = 'django.core.mail.backends.EmailBackend'
-    EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_PORT = 587
-    EMAIL_USE_TLS = True
-    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
-
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
